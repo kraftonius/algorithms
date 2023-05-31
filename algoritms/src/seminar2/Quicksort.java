69391450c5e1322d3a3411615b59dc2a554e6306
kraftonius <avd@yandex.ru>
2023-05-31 20:51:42 +0300

    total fix seminar2

--- algoritms/src/seminar2/Quicksort.java
+++ algoritms/src/seminar2/Quicksort.java
@@ -4,53 +4,55 @@ import java.util.Arrays;
 
 public class Quicksort {
 
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;//завершить выполнение, если длина массива равна 0
 
-    public static void quickSort(int[] var0, int var1, int var2) {
-        if (var0.length != 0) {
-            if (var1 < var2) {
-                int var3 = var1 + (var2 - var1) / 2;
-                int var4 = var0[var3];
-                int var5 = var1;
-                int var6 = var2;
+        if (low >= high)
+            return;//завершить выполнение если уже нечего делить
 
-                while (var5 <= var6) {
-                    while (var0[var5] < var4) {
-                        ++var5;
-                    }
+        // выбрать опорный элемент
+        int middle = low + (high - low) / 2;
+        int opora = array[middle];
 
-                    while (var0[var6] > var4) {
-                        --var6;
-                    }
+        // разделить на подмассивы, который больше и меньше опорного элемента
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < opora) {
+                i++;
+            }
 
-                    if (var5 <= var6) {
-                        int var7 = var0[var5];
-                        var0[var5] = var0[var6];
-                        var0[var6] = var7;
-                        ++var5;
-                        --var6;
-                    }
-                }
+            while (array[j] > opora) {
+                j--;
+            }
 
-                if (var1 < var6) {
-                    quickSort(var0, var1, var6);
-                }
-
-                if (var2 > var5) {
-                    quickSort(var0, var5, var2);
-                }
-
+            if (i <= j) {//меняем местами
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
             }
         }
+
+        // вызов рекурсии для сортировки левой и правой части
+        if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+            quickSort(array, i, high);
     }
 
-    public static void main(String[] var0) {
-        int[] var1 = new int[]{8, 0, 4, 7, 3, 7, 10, 12, -3};
+    public static void main(String[] args) {
+        int[] x = { 8, 0, 4, 7, 3, 7, 10, 12, -3 };
         System.out.println("Было");
-        System.out.println(Arrays.toString(var1));
-        byte var2 = 0;
-        int var3 = var1.length - 1;
-        quickSort(var1, var2, var3);
+        System.out.println(Arrays.toString(x));
+
+        int low = 0;
+        int high = x.length - 1;
+
+        quickSort(x, low, high);
         System.out.println("Стало");
-        System.out.println(Arrays.toString(var1));
+        System.out.println(Arrays.toString(x));
     }
 }
